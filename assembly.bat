0c4af3937ccc4a6dfce8ddad3019061e988ecc61
xPopbobx <48024626+xPopbobx@users.noreply.github.com>
2020-10-30 08:24:16 +0100

    Delete assembly.bat

--- assembly.bat
+++
@@ -1,9 +0,0 @@
-C:\Users\User\AppData\Local\bin\NASM\nasm.exe -o bootsec.bin -f bin bootsec.asm
-C:\Users\User\AppData\Local\bin\NASM\nasm.exe -o kernel.bin -f bin kernel.asm
-copy /b bootsec.bin+kernel.bin disk.bin
-del kernel.bin
-del bootsec.bin
-copy disk.bin bin/disk.bin
-del disk.bin
-cd bin
-bochsrc.bxrc
