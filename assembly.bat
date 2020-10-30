4270bae418c4a4ace7a6e33dc3e69dd79994d1e5
xPopbobx <48024626+xPopbobx@users.noreply.github.com>
2020-10-30 08:34:03 +0100

    Add files via upload

--- assembly.bat
+++ assembly.bat
@@ -1,7 +1,7 @@
 cd src & rem Goto source Folder
 C:\Users\User\AppData\Local\bin\NASM\nasm.exe -o ..\bin\bootsec.bin -f bin bootsec.asm & rem Assembly boot sector
-C:\Users\User\AppData\Local\bin\NASM\nasm.exe -o ..\bin\kernel.bin -f bin kernel.asm & rem Assembly extended assemblys
-copy /b ..\bin\bootsec.bin+..\bin\kernel.bin ..\bin\disk.bin & rem Combine binarys
-del ..\bin\kernel.bin & rem Delete temporary file
+C:\Users\User\AppData\Local\bin\NASM\nasm.exe -o ..\bin\extendedboot.bin -f bin extendedboot.asm & rem Assembly extended assemblys
+copy /b ..\bin\bootsec.bin+..\bin\extendedboot.bin ..\bin\disk.bin & rem Combine binarys
+del ..\bin\extendedboot.bin & rem Delete temporary file
 del ..\bin\bootsec.bin & rem Delete temporary file
 ..\bin\bochsrc.bxrc & rem Debug
